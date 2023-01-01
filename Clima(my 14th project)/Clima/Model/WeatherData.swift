e78c5876114205980969b96f5b5e06e24b4a2f3c
Gabriela Valentina <89490505+GabriellaValentina@users.noreply.github.com>
2023-01-01 19:38:09 +0200

    Add files via upload

---
+++ Clima(my 14th project)/Clima/Model/WeatherData.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Gabriela Valentina on 23.12.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
